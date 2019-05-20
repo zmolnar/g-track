b4ca93df926ac4099ffdc4aa8822c98ee4d11a33
Zoltan Molnar <molnarxzoli@gmail.com>
2019-05-20 22:16:33 +0200

    Implement DC motor driver functionality.

--- software/source/ChainOilerThread.c
+++ software/source/ChainOilerThread.c
@@ -8,13 +8,15 @@
 /*****************************************************************************/
 #include "ChainOilerThread.h"
 #include "ch.h"
+#include "hal.h"
 #include "Dashboard.h"
 #include <string.h>
 
 /*****************************************************************************/
 /* DEFINED CONSTANTS                                                         */
 /*****************************************************************************/
-#define STARTUP_DELAY_IN_SEC   120
+#define STARTUP_DELAY_IN_SEC         120
+#define DEFAULT_SLEEP_DURATION_IN_MS (5*1000)
 
 /*****************************************************************************/
 /* TYPE DEFINITIONS                                                          */
@@ -65,22 +67,54 @@ static double getSpeed(void) {
   return speed;
 }
 
-static time_msecs_t calculatePeriodInMs(double speed) {
-  // TODO: implement algorithm
-  return 60*1000;
+static uint32_t calculatePeriodInMs(double speed) {
+  double sec = 0;
+  if (speed < 10.0)
+    sec = 0;
+  else if ((10.0 <= speed) && (speed < 100.0))
+    sec = (60/9) - speed * 2/3;
+  else
+    sec = 60;
+
+  return (uint32_t)(sec * 1000.0);
 }
 
 static void releaseOilDrop(void) {
-  // TODO: drive DC motor
+  // Leave sleep mode
+  palSetLine(LINE_DCM_SLEEP);
+  chThdSleepMilliseconds(2);
+
+  // Set mode configuration
+  palSetLine(LINE_DCM_AIN1);
+  palClearLine(LINE_DCM_AIN2);
+
+  // Let the pump work
+  chThdSleepMilliseconds(1000);
+
+  // Stop the motor
+  palClearLine(LINE_DCM_AIN1);
+
+  // Enter sleep mode
+  chThdSleepMilliseconds(2);
+  palClearLine(LINE_DCM_SLEEP);
 }
 
 static void handleFireCommand(void) {
     double speed = getSpeed();
-    time_msecs_t periodInMs = calculatePeriodInMs(speed);
+    uint32_t sleepDurationInMs = calculatePeriodInMs(speed);
+    bool dropIsNeeded = true;
+    
+    if (0 == sleepDurationInMs) {
+      dropIsNeeded = false;
+      sleepDurationInMs = DEFAULT_SLEEP_DURATION_IN_MS;
+    }
+
     chSysLock();
-    chVTSetI(&timer, chTimeMS2I(periodInMs), timerCallback, NULL);
+    chVTSetI(&timer, chTimeMS2I(sleepDurationInMs), timerCallback, NULL);
     chSysUnlock();
-    releaseOilDrop();
+    
+    if (dropIsNeeded)
+      releaseOilDrop();
 }
 
 /*****************************************************************************/
