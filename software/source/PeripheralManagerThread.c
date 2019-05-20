b8659d2954792a262e67866810c73b0442e168cb
zmolnar <molnarxzoli@gmail.com>
2019-05-20 10:29:43 +0200

    Add system info file handling to peripheral manager thread.

--- software/source/PeripheralManagerThread.c
+++ software/source/PeripheralManagerThread.c
@@ -11,6 +11,7 @@
 #include "Sdcard.h"
 #include "DebugShell.h"
 #include "sim8xx.h"
+#include <string.h>
 
 /*******************************************************************************/
 /* DEFINED CONSTANTS                                                           */
@@ -37,9 +38,57 @@ static Sim8xxConfig sim_config = {&SD1, &sd_config, LINE_WAVESHARE_POWER};
 /*******************************************************************************/
 /* DEFINITION OF LOCAL FUNCTIONS                                               */
 /*******************************************************************************/
+static void saveBuffer(const char *data, size_t length) {
+  FIL log;
+  if (FR_OK == f_open(&log, "system.nfo", FA_CREATE_ALWAYS | FA_WRITE)) {
+    UINT bw = 0;
+    f_write(&log, data, length, &bw);
+    f_close(&log);
+  }
+}
+
+static void writeSysInfo(void) {
+  char line[128] = {0};
+  char buf[8 * sizeof(line)] = {0};
+
+  chsnprintf(line, sizeof(line), "Kernel:       %s\n", CH_KERNEL_VERSION);
+  strncpy(buf, line, sizeof(buf));
+#ifdef PORT_COMPILER_NAME
+  chsnprintf(line, sizeof(line), "Compiler:     %s\n", PORT_COMPILER_NAME);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+  chsnprintf(line, sizeof(line), "Architecture: %s\n", PORT_ARCHITECTURE_NAME);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#ifdef PORT_CORE_VARIANT_NAME
+  chsnprintf(line, sizeof(line), "Core Variant: %s\n", PORT_CORE_VARIANT_NAME);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+#ifdef PORT_INFO
+  chsnprintf(line, sizeof(line), "Port Info:    %s\n", PORT_INFO);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+#ifdef PLATFORM_NAME
+  chsnprintf(line, sizeof(line), "Platform:     %s\n", PLATFORM_NAME);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+#ifdef BOARD_NAME
+  chsnprintf(line, sizeof(line), "Board:        %s\n", BOARD_NAME);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+#ifdef __DATE__
+#ifdef __TIME__
+  chsnprintf(line, sizeof(line), "Build time:   %s - %s\n", __DATE__, __TIME__);
+  strncpy(buf + strlen(buf), line, sizeof(buf) - strlen(buf));
+#endif
+#endif
+
+  saveBuffer(buf, strlen(buf));
+}
+
 static void sdcardInsertedHandler(eventid_t id) {
   (void)id;
   sdcardMount();
+  writeSysInfo();
 }
 
 static void sdcardRemovedHandler(eventid_t id) {
