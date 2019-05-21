8309fd0ef76da74a269515d4a7cd42897f0a6fbd
zmolnar <molnarxzoli@gmail.com>
2019-05-21 10:05:39 +0200

    Add day-of-week calculation
    Implement Zeller's congruence algorithm.

--- software/source/GpsReaderThread.c
+++ software/source/GpsReaderThread.c
@@ -100,10 +100,25 @@ static void restoreChar(char *c, char *tmp) {
   *c = *tmp; 
 }
 
-static int dayOfWeek(int y, int m, int d) {
-  #warning Calculate day-of-week correctly
-  //return ((y -= m < 3) + y / 4 - y / 100 + y / 400 + "-bed=pen+mad."[m] + d) % 7;
-  return 0;
+static int dayOfWeek(int year, int month, int day) {
+  if(month == 1) {
+    month = 13;
+    year--;
+  }
+  
+  if (month == 2) {
+    month = 14;
+    year--;
+  }
+  
+  int q = day;
+  int m = month;
+  int k = year % 100;
+  int j = year / 100;
+  int h = q + 13*(m+1)/5 + k + k/4 + j/4 + 5*j;
+  h %= 7;
+ 
+  return h;
 }
 
 static bool isDaylightSavingTime(int day, int month, int dow) {
